5e1a99097e10311aa7687d4d5f44251323ca59df
Jonathan Zarate <zaratedev@gmail.com>
2024-03-05 16:02:54 -0600

    Install axios, fetch users and shade rows

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [shadedRows, setShadedRows] = useState(false);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('https://randomuser.me/api/?results=100');
+        setUsers(response.data.results);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const toggleShadedRows = () => {
+    setShadedRows(prevState => !prevState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista de usuarios</h1>
+      <button onClick={toggleShadedRows}>Alternar Colores</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Género</th>
+            <th>Email</th>
+            <th>Teléfono</th>
+            <th>Ciudad</th>
+            <th>País</th>
+            <th>Foto</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={user.login.uuid} style={{backgroundColor: shadedRows && index % 2 === 0 ? '#112233' : (shadedRows ? '#556677' : 'transparent')}}>
+              <td>{`${user.name.first} ${user.name.last}`}</td>
+              <td>{user.gender}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>{user.location.city}</td>
+              <td>{user.location.country}</td>
+              <td><img src={user.picture.thumbnail} alt="Avatar" /></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
